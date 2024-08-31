4dc10a484ab9d1bab57f2b12c237333a7ee76559
Dias Kalkamanov <diicellman@gmail.com>
2024-09-01 01:56:15 +0500

    pass stage

--- src/main.zig
+++ src/main.zig
@@ -3,16 +3,18 @@ const std = @import("std");
 pub fn main() !void {
     // Uncomment this block to pass the first stage
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("$ ", .{});
 
-    const stdin = std.io.getStdIn().reader();
-    var buffer: [1024]u8 = undefined;
-    const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+    while (true) {
+        try stdout.print("$ ", .{});
 
-    // TODO: Handle user input
-    // test
-    // _ = user_input;
-    if (user_input.len > 0) {
-        try stdout.print("{s}: command not found", .{user_input});
+        const stdin = std.io.getStdIn().reader();
+        var buffer: [1024]u8 = undefined;
+        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+
+        // TODO: Handle user input
+        // _ = user_input;
+        if (user_input.len > 0) {
+            try stdout.print("{s}: command not found\n", .{user_input});
+        }
     }
 }
