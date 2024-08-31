1e317c48214a6e4155dd02f18b44a5a0e5e4d687
Dias Kalkamanov <diicellman@gmail.com>
2024-09-01 01:41:49 +0500

    pass 1st stage

--- src/main.zig
+++ src/main.zig
@@ -2,13 +2,14 @@ const std = @import("std");
 
 pub fn main() !void {
     // Uncomment this block to pass the first stage
-    // const stdout = std.io.getStdOut().writer();
-    // try stdout.print("$ ", .{});
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("$ ", .{});
 
     const stdin = std.io.getStdIn().reader();
     var buffer: [1024]u8 = undefined;
     const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
 
     // TODO: Handle user input
+    // test
     _ = user_input;
 }
