bf2770b34fd7adda3e0337eea950279eff2db2fb
Dias Kalkamanov <diicellman@gmail.com>
2024-09-02 00:11:52 +0500

    pass stage

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,25 @@
 const std = @import("std");
 
+const Command = union(enum) {
+    Exit,
+    // Help,
+    // Echo: []const u8,
+    Unknown: []const u8,
+
+    fn parse(input: []const u8) Command {
+        const trimmed = std.mem.trim(u8, input, &std.ascii.whitespace);
+        if (std.mem.eql(u8, trimmed, "exit")) {
+            return Command.Exit;
+            // } else if (std.mem.eql(u8, trimmed, "help")) {
+            //     return Command.Help;
+            // } else if (std.mem.startsWith(u8, trimmed, "echo ")) {
+            //     return Command{ .Echo = trimmed[5..] };
+        } else {
+            return Command{ .Unknown = trimmed };
+        }
+    }
+};
+
 pub fn main() !void {
     // Uncomment this block to pass the first stage
     const stdout = std.io.getStdOut().writer();
@@ -14,9 +34,18 @@ pub fn main() !void {
         // TODO: Handle user input
         // _ = user_input;
         if (user_input.len > 0) {
-            try stdout.print("{s}: command not found\n", .{user_input});
+            const command = Command.parse(user_input);
+            switch (command) {
+                .Exit => {
+                    std.process.exit(0);
+                },
+                .Unknown => |cmd| {
+                    try stdout.print("{s}: command not found\n", .{cmd});
+                },
+            }
+            // try stdout.print("{s}: command not found\n", .{user_input});
             // try stdout.print("$ exit 0", .{});
-            std.process.exit(0);
+            // std.process.exit(0);
         }
     }
 }
